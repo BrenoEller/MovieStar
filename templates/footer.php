b568dfb002a0944ab6d63508200c273a8b8e5f7a
Breno <brenoeller@hotmail.com>
2023-04-02 16:14:04 -0300

    up - separação dos templates footer e header

---
+++ templates/footer.php
@@ -0,0 +1,31 @@
+
+<footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li id="list-footer">
+                    <a href="#">Adicionar filme</a>
+                    <a href="#">Adicionar crítica</a>
+                    <a href="#">Entrar / Registrar</a>
+                </li>
+            </ul>
+        </div>
+
+        <p>&copy; Hora de Codar</p>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
