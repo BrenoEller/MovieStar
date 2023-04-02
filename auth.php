9d5fe84efc3aa9601614e84e5f2d7f5da5c50c0f
Breno <brenoeller@hotmail.com>
2023-04-02 16:50:48 -0300

    new - estilização e criação da aba auth.php para registo e login do usuario

--- auth.php
+++ auth.php
@@ -5,7 +5,52 @@
     ?>
 
     <div id="main-container" class="container-fluid">
-        Autenticação
+        <div class="col-md-12">
+            <div class="row" id="auth-row">
+                <div class="col-md-4" id="login-container">
+                    <h2>Entrar</h2>
+                    <form action="" method="POST">
+                        <input type="hidden" value="login" name="type">
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" class="form-control" placeholder="Digite seu email" id="email" name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Senha</label>
+                            <input type="password" class="form-control" placeholder="Digite sua senha" id="password" name="password">
+                        </div>
+                        <input type="submite" class="btn card-btn" value="Entrar">
+                    </form>
+                </div>
+                <div class="col-md-4" id="register-container">
+                    <h2>Criar conta</h2>
+                    <form action="" method="POST">
+                        <input type="hidden" value="register" name="type">
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" class="form-control" placeholder="Digite seu email" id="email" name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Nome</label>
+                            <input type="text" class="form-control" placeholder="Digite seu nome" id="name" name="name">
+                        </div>
+                        <div class="form-group">
+                            <label for="lastname">Sobrenome</label>
+                            <input type="text" class="form-control" placeholder="Digite seu sobrenome" id="lastname" name="lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Senha</label>
+                            <input type="password" class="form-control" placeholder="Digite sua senha" id="password" name="password">
+                        </div>
+                        <div class="form-group">
+                            <label for="confirmpassword">Confirme sua senha</label>
+                            <input type="password" class="form-control" placeholder="Confirme sua senha" id="confirmpassword" name="confirmpassword">
+                        </div>
+                        <input type="submite" class="btn card-btn" value="Registrar">
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
 <?php 
