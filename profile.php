b30f29d37f0ee1a62fc25605c15885379ea8d3e1
Breno <brenoeller@hotmail.com>
2023-05-04 00:12:37 -0300

    new - tela do usuário

--- profile.php
+++ profile.php
@@ -6,6 +6,7 @@
 
     $user = new User();
     $userDao = new UserDAO($conn, $BASE_URL);
+    $movieDao = new MovieDAO($conn, $BASE_URL);
 
     $id = filter_input(INPUT_GET, "id");
 
@@ -27,10 +28,41 @@
             $message->setMessage("Usuário não encontrado", "error", "index.php");
         }
     }
+
+    $fullName = $user->getFullName($userData);
+
+    if($userData->image == "") {
+        $userData-> image = "user.png";
+    }
+
+    $userMovies = $movieDao->getMoviesByUserId($id);
+    
 ?>
 
 <div id="main-container" class="container-fluid">
-    
+    <div class="col-md-8 offset-md-2">
+        <div class="row profile-container">
+            <div class="col-md-12">
+                <h1 class="page-title"><?= $fullName ?></h1>
+                <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>imagens/users/<?= $userData->image ?>')"></div>
+                <h3 class="about-title">Sobre:</h3>
+                <?php if(!empty($userData->bio)): ?>
+                    <p class="profile-description"><?= $userData->bio ?></p>
+                <?php else: ?>
+                    <p class="profile-description">O usuário ainda não escreveu a bio.</p>
+                <?php endif; ?>
+            </div>
+            <div class="col-md-12 added-movies-container">
+                <h3>Filmes do usuário:</h3>
+                <?php foreach($userMovies as $movies): ?>
+                    <?php require("templates/movie_card.php"); ?>
+                <?php endforeach; ?>
+                <?php if(count($movies) === 0): ?>
+                    <p class="empty-list">O usuário ainda não enviou filmes.</p>
+                    <?php endif; ?>
+            </div>
+        </div>
+    </div>
 </div>
 
 
