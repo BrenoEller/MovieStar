b3b170014fe8a8138e740c5941dd97b919a04a67
Breno <brenoeller@hotmail.com>
2023-05-03 23:12:57 -0300

    new - editprofile front-end

---
+++ profile.php
@@ -0,0 +1,34 @@
+<?php 
+    require_once("templates/header.php");
+    require_once("dao/UserDAO.php");
+    require_once("models/User.php");
+    require_once("dao/MovieDAO.php");
+
+    $user = new User();
+    $userDao = new UserDAO($conn, $BASE_URL);
+
+    $id = filter_input(INPUT_GET, "id");
+
+    if(empty($id)) {
+
+        if(!empty($userData)) {
+
+            $id = $userData->id;
+        } else {
+
+            $message->setMessage("Usuário não encontrado", "error", "index.php");
+        }
+    } else {
+
+        $message->setMessage("Usuário não encontrado", "error", "index.php");
+    }
+?>
+
+<div id="main-container" class="container-fluid">
+    
+</div>
+
+
+<?php 
+    require_once("templates/footer.php")
+?>
