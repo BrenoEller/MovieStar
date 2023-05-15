80f9d2d2af284acfd82ac0a568429c513562d033
Breno <brenoeller@hotmail.com>
2023-05-15 06:09:14 -0300

    new - dados do usuário ao inserir crítica ao filme

--- templates/user_review.php
+++ templates/user_review.php
@@ -1,11 +1,24 @@
+<?php 
+
+    require_once("models/User.php");
+
+    $userModel = new User();
+    $fullName = $userModel->getFullName($review->user);
+
+    if($review->user->image == "") {
+
+        $review->user->image = "user.png";
+    }
+
+?>
 <div class="col-md-12 review">
     <div class="row">
         <div class="col-md-1">
-                <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>imagens/users/user.png')"></div>
+                <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>imagens/users/<?= $review->user->image ?>')"></div>
         </div>
         <div class="col-md-9 author-details-container">
             <h4 class="author-name">
-                <a href="">Nome do usuário</a>
+                <a href=""><?= $fullName ?></a>
             </h4>
             <p><i class="fas fa-star"></i> <?= $review->rating ?></p>
         </div>
